99da42fc9a1c5ccb341ec1bb7b014222a19908d8
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2023-12-12 19:30:23 -0300

    feat: verify user behavior

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,27 @@
-import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import UserInterface._
+
 
 object Main {
-  import Semaphore._
   def main(args: Array[String]): Unit = {
-    Helper(5, 1)
+    val user = ActorSystem(spawnUser(1), "user")
+    val system = Behaviors.setup[String] {
+      context =>
+        def log(str: String): Unit = {
+          context.log.info(s"[${context.self.path.name}] Received: $str")
+          user ! passSomeWork(1, context.self) {
+            state => s"Received the user state: $state"
+          }
+        }
+        Behaviors.receiveMessage[String] {
+          message => {
+            log(message)
+            Behaviors.same
+          }
+        }
     }
+    val systemActor = ActorSystem(system, "system")
+    systemActor ! "Hello World!"
+  }
 }
---
+++ src/main/scala/UserInterface.scala
@@ -0,0 +1,50 @@
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
+
+object UserInterface {
+  case class State(lc: Int) {
+    def tick: State = State(lc + 1)
+    def tick(ts: Int): State = State(math.max(lc, ts) + 1)
+    def send[T](data: T)(implicit replyTo: ActorRef[T]): State = {
+      replyTo ! data
+      tick
+    }
+  }
+  sealed trait Section
+  case class Critical(ts: Int) extends Section
+  case class NonCritical(ts: Int) extends Section
+  case class DoSomething[T](ts: Int, work: State => T, replyTo: ActorRef[T]) extends Section
+
+  def passSomeWork[T](ts: Int, replyTo: ActorRef[T])(work: State => T) =
+    DoSomething[T](ts, work, replyTo)
+  def spawnUser(id: Long): Behavior[Section] = Implementation(id)
+
+  private object Implementation {
+    private def behavior(state: State)(implicit defaultProc: State => Unit): Behaviors.Receive[Section] = {
+      defaultProc(state)
+      Behaviors.receiveMessage[Section] {
+        case Critical(ts) => behavior {
+          state.tick(ts)
+        }
+        case DoSomething(ts, work, replyTo) => behavior {
+          state.send {
+            work(state)
+          }(replyTo)
+          state.tick(ts)
+        }
+      }
+    }
+
+    def apply(id: Long): Behavior[Section] = {
+      Behaviors.setup[Section] {
+        context =>
+          def log(state: State): Unit = {
+            context.log.info(s"[$id]Node ${context.self.path.name} state: $state")
+            Thread.sleep(1000)
+          }
+
+          behavior(State(0))(log)
+      }
+    }
+  }
+}
