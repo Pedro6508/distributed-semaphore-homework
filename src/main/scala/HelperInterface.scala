2304f36d8986039d3401f6204b8574bd5e0051e6
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2023-12-13 14:04:47 -0300

    broke: helper state management

--- src/main/scala/HelperInterface.scala
+++ src/main/scala/HelperInterface.scala
@@ -3,55 +3,65 @@ import UserInterface.{Critical, NonCritical}
 import akka.actor.typed.scaladsl._
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
+import akka.io.Udp.Message
 
 object HelperInterface {
+  case class SemOp(kind: SemaphoreCommand, id: Long, ts: Int) {
+    def updateMap(map: Map[Long, SemOp]): Map[Long, SemOp] = map + (id -> this)
+  }
+
   sealed trait SemaphoreCommand
-  case class VOP(ts: Int) extends SemaphoreCommand
-  case class POP(ts: Int) extends SemaphoreCommand
-  case class ACK(ts: Int) extends SemaphoreCommand
+  case class VOP() extends SemaphoreCommand
+  case class POP() extends SemaphoreCommand
 
   sealed trait HelperCommand
 
   sealed trait NetworkCommand extends HelperCommand
   case class Connect(firstTick: Int, network: List[ActorRef[NetworkCommand]]) extends NetworkCommand
-  case class Broadcast(message: SemaphoreCommand) extends NetworkCommand
+  case class Broadcast(op: SemOp) extends NetworkCommand
+  case class ACK(ts: Int, Command: SemaphoreCommand) extends NetworkCommand
 
   sealed trait UserReq extends HelperCommand
-  case class ReqV(ts: Int) extends HelperCommand
-  case class ReqP(ts: Int) extends HelperCommand
+  case class ReqV() extends HelperCommand
+  case class ReqP() extends HelperCommand
 
   def spawnHelper(id: Long): Behavior[HelperCommand] = Implementation(id)
 
   private object Implementation {
-    case class SharedState(mq: List[SemaphoreCommand], network: List[ActorRef[NetworkCommand]])
-
-    private def broadcast(state: ClockState)(message: SemaphoreCommand)
-     (implicit log: ActorLogger, shared: SharedState): Behavior[HelperCommand] = {
-      message match {
-        case VOP(ts) => behavior {
-          log.log("Broadcast VOP")
-          state.tick(ts)
-        }
-        case POP(ts) => behavior {
-          log.log("Broadcast POP")
-          state.tick(ts)
-        }
-        case ACK(ts) => behavior {
-          log.log("Broadcast ACK")
-          state.tick(ts)
-        }
-      }
+    case class Shared(s: Int, flyAck: List[SemOp], network: List[ActorRef[NetworkCommand]])
+    case class State(clock: ClockState, shared: Shared, unordered: Map[Long, SemOp]) { // TODO: PATCH AND COMMIT
+      def mapClock(f: ClockState => ClockState) = copy(clock = f(clock))
+      def mapShared(f: Shared => Shared) = copy(shared = f(shared))
+      def mapUnordered(f: Map[Long, SemOp] => Map[Long, SemOp]) = copy(unordered = f(unordered))
     }
 
-    private def behavior(state: ClockState)(implicit log: ActorLogger, shared: SharedState): Behavior[HelperCommand] = {
-      log.log(state)
+    // TODO: Define the different types of broadcast
+    // Broadcast is the normal behavior
+    // receive reqV -> broadcast VOP
+    // receive reqP -> broadcast POP
+    // receive POP v VOP -> broadcast ACK
+    def broadcast(state: State)(op: SemOp) = {
+      (state.unordered.get(op.id) match {
+        case Some(_) =>
+          state.mapUnordered(_.removed(op.id))
+            .mapShared { shared => shared.copy(flyAck = op :: shared.flyAck) }
+          state.mapClock(_.send(ACK(state.c))) // TODO: ADD ACK SEND TO LOGICAL CLOCK
+        case None =>
+          state.mapUnordered(op.updateMap)
+      }).mapClock(_.tick)
+    }
+
+    // TODO: Broadcast every step, except the "{ send GO }"
+    private def behavior(state: State)(implicit log: ActorLogger): Behavior[HelperCommand] = {
+      log.log(state.clock)
       Behaviors.receiveMessage[HelperCommand] {
-        case Connect(firstTick: Int, network) => behavior {
+        case Connect(firstTick: Int, network) => behavior{
           log.log("Connect")
-          SharedState(List(), network)
-          state.tick(firstTick)
+          state.copy(
+            shared = state.shared.copy(network = network),
+            clock = state.clock.tick(firstTick)
+          )
         }
-        case Broadcast(message) => broadcast(state)(message)
         case ReqV(ts) => behavior {
           log.log("Broadcast VOP")
           state.tick(ts)
@@ -68,9 +78,11 @@ object HelperInterface {
         context =>
           val user = UserInterface.spawnUser(id, context.self)
           val logger = ActorLogger(context, "helper")
+          val shared = SharedState(List(), List())
           user ! NonCritical(0)
+          user ! Critical(0)
 
-          behavior(ClockState(0))(logger, SharedState(List(), List()))
+          behavior(shared)(ClockState(0))(logger)
       }
     }
   }
